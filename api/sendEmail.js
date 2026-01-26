92319757437055625e79f70cf13d1c0c31a515b0
webvertize-ro <contact@webvertize.ro>
2026-01-26 04:08:00 +0200

    implementing form

--- api/sendEmail.js
+++ api/sendEmail.js
@@ -6,7 +6,7 @@ export default function handler(req, res) {
   const { name, phone, email, message } = req.body;
 
   if (!name || !phone || !email) {
-    return res.sttus(400).json({ status: 'Missing required fields!' });
+    return res.status(400).json({ status: 'Missing required fields!' });
   }
 
   res.status(200).json({ success: true, message: 'Received data' });
