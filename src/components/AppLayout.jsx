c20fc39876e251b3c3554a4c653559e547eeb741
webvertize-ro <contact@webvertize.ro>
2026-05-04 15:36:06 +0300

    further changes

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -1,14 +1,28 @@
-import Footer from './Footer';
-import { Outlet } from 'react-router';
-import Navigation from './Navigation';
-import SideButtons from './SideButtons';
-import BackTopBtn from './BackTopBtn';
-import CookiePopup from './CookiePopup';
-import BottomNav from './BottomNav';
+import Footer from "./Footer";
+import { Outlet } from "react-router";
+import Navigation from "./Navigation";
+import SideButtons from "./SideButtons";
+import BackTopBtn from "./BackTopBtn";
+import CookiePopup from "./CookiePopup";
+import BottomNav from "./BottomNav";
+import c from "../../utils/content";
+import { useContent } from "../hooks/useContent";
+import { Helmet } from "react-helmet-async";
 
 function AppLayout() {
+  const { contentMap } = useContent();
+
   return (
     <div>
+      <Helmet>
+        {c(contentMap, "global.favicon") && (
+          <link
+            rel="icon"
+            type="image/png"
+            href={c(contentMap, "global.favicon")}
+          />
+        )}
+      </Helmet>
       <Navigation />
       <div role="main">
         <Outlet />
