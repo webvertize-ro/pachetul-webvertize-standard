4cac8cc7f7e1542a56e2699ceda2a0bbda842431
webvertize-ro <contact@webvertize.ro>
2026-08-25 02:09:44 +0300

    implemented logo sizes modifications

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,19 +2,28 @@ import styled from "styled-components";
 import c from "../../utils/content";
 import { useContent } from "../hooks/useContent";
 
+const DEFAULT_WIDTH = 160;
+const MIN_WIDTH = 40;
+const MAX_WIDTH = 400;
+
 const StyledImg = styled.img`
-  max-width: ${(props) => (props.width ? props.width : "160px")};
-  max-height: ${(props) => (props.width ? props.width : "60px")};
+  width: ${(props) => props.$width}px;
+  height: auto;
+  display: block;
 `;
 
-function Logo({ width }) {
+function Logo() {
   const { contentMap } = useContent();
+  const rawWidth = parseInt((c(contentMap, "global.logo_width"), 10));
+  const width =
+    Number.isFinite(rawWidth) && rawWidth >= MIN_WIDTH && rawWidth <= MAX_WIDTH
+      ? rawWidth
+      : DEFAULT_WIDTH;
 
   return (
     <StyledImg
-      width={width}
+      $width={width}
       src={c(contentMap, "global.logo")}
-      b
       alt="Logo-ul afacerii [Nume Afacere]"
     />
   );
