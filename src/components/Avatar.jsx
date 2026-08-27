a0bf678fd87baa3643eaa0cb1d3962bcf0bd75c8
webvertize-ro <contact@webvertize.ro>
2026-08-27 20:49:13 +0300

    edited user images in testimonials

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
 const StyledImg = styled.img`
-  max-width: 80px;
-  max-height: 80px;
+  width: 80px;
+  height: 80px;
   border-radius: 50%;
   border: 2px solid #ccc;
   object-fit: cover;
